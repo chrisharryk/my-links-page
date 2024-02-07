3c7a10cf788212588ed445c3e01ae31bb22b55e0
chrisharryk <113275412+chrisharryk@users.noreply.github.com>
2024-02-07 14:01:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+VANTA.BIRDS({
+    el: "*",
+    quantity: 4,
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    birdSize: 0.75,
+    scaleMobile: 1.00,
+    backgroundAlpha: 0.00
+})
